32e2f9a8ec813d610514b49f1ce025541fca6642
SeanDuffie <duffiesean@gmail.com>
2026-03-26 23:50:03 -0500

    Fix webpage by specifying littlefs filesystem type

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 512, // Compress anything larger than 512 bytes
+      deleteOriginFile: true // Delete the uncompressed files to save ESP32 flash space
+    })
+  ],
+  base: './', // Forces Vite to use relative paths for CSS and JS assets
   build: {
     // Output directly to the PlatformIO data folder
     outDir: '../data', 
